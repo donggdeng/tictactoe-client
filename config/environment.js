e9477d5901df0fe915ac6c589c4535a9dc0d130d
Deng <donggdeng@gmail.com>
2023-04-23 19:15:59 +0800

    Config api host

--- config/environment.js
+++ config/environment.js
@@ -26,6 +26,12 @@ module.exports = function (environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV['ember-cli-mirage'] = {
+      enabled: false,
+    };
+
+    ENV.apiHost = 'http://localhost:3000';
   }
 
   if (environment === 'test') {
