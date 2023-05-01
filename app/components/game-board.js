f24547f328b578a8c7a05bcd6e2a8cd481730b59
Dong Deng <deng@ekohe.com>
2023-05-01 11:41:46 +0800

    Alert and disable clicks when game finished

--- app/components/game-board.js
+++ app/components/game-board.js
@@ -27,7 +27,12 @@ export default class GameBoard extends Component {
   @action
   async playCell(event) {
     const cell = event.target;
+    const gameBoardElement = cell.parentNode.parentNode;
     const position = cell.dataset.position;
+
+    if (gameBoardElement.classList.contains('finished')) {
+      return;
+    }
 
     if (cell.classList.contains('played')) {
       return;
@@ -45,9 +50,9 @@ export default class GameBoard extends Component {
     try {
       await newMove.save();
 
-      cell.innerText = this.isPlayer1Turn ? 'x' : 'o';
-      cell.classList.add('played');
+      this.updateCell(cell);
       this.args.game.moves.pushObject(newMove);
+      this.updateGameStatus(gameBoardElement);
     } catch (error) {
       console.log(error);
     }
@@ -56,5 +61,29 @@ export default class GameBoard extends Component {
   @action
   restartGame() {
     window.location.reload(true);
+  }
+
+  updateCell(cell) {
+    cell.innerText = this.isPlayer1Turn ? 'x' : 'o';
+    cell.classList.add('played');
+  }
+
+  async updateGameStatus(gameBoardElement) {
+    const game = await this.args.game.reload();
+
+    if (game.status == 'finished') {
+      gameBoardElement.classList.add('finished');
+      alert(`Game over! ${this.alertMessage(game.winner)}`);
+    }
+  }
+
+  alertMessage(winner) {
+    if (winner == 'player1') {
+      return 'Player 1 won!';
+    } else if (winner == 'player2') {
+      return 'Player 2 won!';
+    } else {
+      return 'Draw!';
+    }
   }
 }
